844e75bceacf7e4e7327fa04838dc48b0b844ab5
Howard_Nguyen <cs61c-ut@hive5.CS.Berkeley.EDU>
2013-10-23 00:40:52 -0700

    nothing really

--- part1.c
+++ part1.c
@@ -11,12 +11,14 @@ int conv2D(float* in, float* out, int data_size_X, int data_size_Y,
     
     // main convolution loop
 	for(int x = 0; x < data_size_X; x++){ // the x coordinate of the output location we're focusing on
-		for(int y = 0; y < data_size_Y; y++){ // the y coordinate of theoutput location we're focusing on
-			for(int i = -kern_cent_X; i <= kern_cent_X; i++){ // kernel unflipped x coordinate
+		for(int i = -kern_cent_X; i <= kern_cent_X; i++){ // kernel unflipped x coordinate
+			for(int y = 0; y < data_size_Y; y++){ // the y coordinate of theoutput location we're focusing on
 				for(int j = -kern_cent_Y; j <= kern_cent_Y; j++){ // kernel unflipped y coordinate
 					// only do the operation if not out of bounds
 					if(x+i>-1 && x+i<data_size_X && y+j>-1 && y+j<data_size_Y){
 						//Note that the kernel is flipped
+						//_mm_loadu_si128((kern_cent_X-i)+(kern_cent_Y-j)*KERNX);
+						//_mm_loadu_si128((x+i) + (y+j)*data_size_X);
 						out[x+y*data_size_X] += 
 								kernel[(kern_cent_X-i)+(kern_cent_Y-j)*KERNX] * in[(x+i) + (y+j)*data_size_X];
 					}
